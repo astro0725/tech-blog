a535ed09d1eedc5facbaec1fd4f8c7629558cb14
astro <angelica.strong0725@gmail.com>
2024-02-13 10:06:37 -0800

    function to render all existing posts

---
+++ controllers/home.js
@@ -0,0 +1,31 @@
+const db = require('../models');
+const Post = db.post;
+const User = db.user;
+
+async function renderHome(req, res) {
+  try {
+    const posts = await Post.findAll({
+      include: [
+        {
+          model: User,
+          attributes: ['username']
+        }
+      ],
+      order: ['createdAt', 'DESC']
+    });
+
+    const postsPlain = posts.map(post => post.get({ plain: true }));
+
+    res.render('home', {
+      posts: postsPlain,
+    });
+
+  } catch (error) {
+    console.error('Error fetching posts: ', error);
+    res.status(500).send('Error fetching posts');
+  }
+}
+
+module.exports = {
+  renderHome
+},

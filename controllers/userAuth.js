60df4654969874fe5bbd5a40a8e6d34f2356bc36
astro <angelica.strong0725@gmail.com>
2024-02-13 01:19:04 -0800

    changed quotes

--- controllers/userAuth.js
+++ controllers/userAuth.js
@@ -1,57 +1,57 @@
-const db = require("../models");
+const db = require('../models');
 const User = db.User;
 // const Session = db.Session;
 
 // function to sign up a new user with username and password
 async function signUpUser(req, username, password) {
-  console.log("Received request body:", req.body);
+  console.log('Received request body:', req.body);
 
   try {
     // create a new user record in the database
     const newUser = await User.create({ username, password });
-    console.log("New User:", newUser.toJSON());
+    console.log('New User:', newUser.toJSON());
 
     // store session record in the database
     req.session.authenticated = true;
     req.session.user_id = newUser.id;
 
-    console.log("Session ID:", req.session.id);
-    console.log ("User ID:", req.session.user_id);
+    console.log('Session ID:', req.session.id);
+    console.log ('User ID:', req.session.user_id);
 
-    console.log("User registered:", newUser);
+    console.log('User registered:', newUser);
     return {success: true};
 
   } catch (error) {
-    console.log("Error:", error);
+    console.log('Error:', error);
     return {success: false, error: error};
   }
 }
 
 // function to login existing user with username and password
 async function loginUser(req, username, password) {
-  console.log("Received request body:", req.body);
+  console.log('Received request body:', req.body);
 
   try {
     // find the user record in the database
     const user = await User.findOne({ where: { username: username } });
-    console.log("User:", user.toJSON());
+    console.log('User:', user.toJSON());
 
     // if the user record is found, compare the password
     if (user && user.password === password) {
-      console.log("User authenticated:", user);
+      console.log('User authenticated:', user);
       req.session.authenticated = true;
       req.session.user_id = user.id;
-      console.log("Session ID:", req.session.id);
-      console.log ("User ID:", req.session.user_id);
+      console.log('Session ID:', req.session.id);
+      console.log ('User ID:', req.session.user_id);
       return {success: true};
 
     } else {
-      console.log("User not authenticated");
-      return {success: false, error: "Invalid username or password"};
+      console.log('User not authenticated');
+      return {success: false, error: 'Invalid username or password'};
     }
 
   } catch (error) {
-    console.log("Error:", error);
+    console.log('Error:', error);
     return {success: false, error: error};
   }
 }
@@ -65,20 +65,20 @@ async function logoutUser(req) {
         // destroy the user's session
         req.session.destroy(err => {
           if (err) {
-            console.error("Error destroying session:", err);
-            reject("Error destroying session.");
+            console.error('Error destroying session:', err);
+            reject('Error destroying session.');
           } else {
-            console.log("User signed out successfully.");
+            console.log('User signed out successfully.');
             resolve({ success: true });
           }
         });
       } else {
-        console.error("No active session to log out.");
-        reject("No active session.");
+        console.error('No active session to log out.');
+        reject('No active session.');
       }
     } catch (error) {
-      console.error("Error during logout:", error);
-      reject("Error during logout.");
+      console.error('Error during logout:', error);
+      reject('Error during logout.');
     }
   });
 }

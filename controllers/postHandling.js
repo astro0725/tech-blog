5ad4f58a823ce7087797d12d5319bae7ffc1dd8d
astro <angelica.strong0725@gmail.com>
2024-02-13 01:21:50 -0800

    create post function

--- controllers/postHandling.js
+++ controllers/postHandling.js
@@ -2,7 +2,21 @@ const db = require('../models');
 const Post = db.Post;
 
 async function createPost(req, title, body) {
+  if (!req.session.user_id) {
+    throw new Error('You must be logged in to create a post');
+  }
 
+  try {
+    const post = await Post.create({
+      title,
+      body,
+      user_id: req.session.user_id,
+    });
+    return {success: true, post};
+  } catch (error) {
+    console.error('Error creating post:', error);
+    return {success: false, error: error.message};
+  }
 }
 
 async function editPost(req, title, body) {

5737fcd7055707216a559aaa3502749a41deafb8
astro <angelica.strong0725@gmail.com>
2024-02-13 10:54:55 -0800

    get post by id

--- controllers/postHandling.js
+++ controllers/postHandling.js
@@ -1,5 +1,6 @@
 const db = require('../models');
 const Post = db.Post;
+const User = db.User;
 
 async function createPost(req, title, body) {
   if (!req.session.user_id) {
@@ -17,7 +18,7 @@ async function createPost(req, title, body) {
     console.error('Error creating post:', error);
     return {success: false, error: error.message};
   }
-}
+};
 
 async function editPost(req, title, body) {
   if (!req.session.user_id) {
@@ -46,8 +47,7 @@ async function editPost(req, title, body) {
     console.error('Error editing post:', error);
     return {success: false, error: error.message};
   }
-
-}
+};
 
 async function deletePost(req) {
   if (!req.session.user_id) {
@@ -74,12 +74,34 @@ async function deletePost(req) {
     console.error('Error deleting post:', error);
     return {success: false, error: error.message};
   }
+};
 
-}
+async function getPostById(req) {
+  try {
+    const post = await Post.findOne({
+      where: { id: req.params.id },
+      include: [
+        {
+          model: User,
+          attributes: ['username'],
+        },
+      ],
+    });
 
+    if (!post) {
+      throw new Error('Post not found');
+    }
+
+    return {success: true, post};
+  } catch (error) {
+    console.error('Error getting post by id:', error);
+    return {success: false, error: error.message};
+  }
+};
 
 module.exports = {
   createPost,
   editPost,
-  deletePost
+  deletePost,
+  getPostById,
 }

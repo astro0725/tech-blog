22b26284c57b54aa1fc81c7ebba2675811a5ac05
astro <angelica.strong0725@gmail.com>
2024-02-13 01:14:36 -0800

    post template

---
+++ controllers/postHandling.js
@@ -0,0 +1,21 @@
+const db = require('../models');
+const Post = db.Post;
+
+async function createPost(req, title, body) {
+
+}
+
+async function editPost(req, title, body) {
+
+}
+
+async function deletePost(req) {
+
+}
+
+
+module.exports = {
+  createPost,
+  editPost,
+  deletePost
+}
